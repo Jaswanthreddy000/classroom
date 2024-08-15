cc138d09773ab3ae73995ae9e5e3f865f26bba4e
Jaswanthreddy000 <116836018+Jaswanthreddy000@users.noreply.github.com>
2024-08-15 10:18:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin : [https://classroom-1-uigw.onrender.com],
+    origin : 'https://classroom-1-uigw.onrender.com',
     methods : ["POST", "GET"],
     credentials : true
   }
